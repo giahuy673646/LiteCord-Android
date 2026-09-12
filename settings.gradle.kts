e578e795010dfca3fb9c4837deba178de02b3f22
giahuy673646 <giahuy673646@gmail.com>
2026-09-12 13:31:04 +0700

    Add gradle settings with repositories

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "LiteCord"
+include(":app")
+include(":core")
+include(":network")
+include(":ui")
